777ba3ada4ddd8f9ac077e4f2eaa645a66cd61c0
Udit Bhardwaj <114299379+TheUditBhardwaj@users.noreply.github.com>
2025-07-08 15:57:14 +0530

    Starting

---
+++ foodmarcos/android/app/src/main/kotlin/com/example/foodmarcos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodmarcos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ foodmarcos/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/food_recognition_service.dart';
+import 'screens/home_screen.dart';
+import 'providers/food_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FoodProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Food Macros Finder',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
